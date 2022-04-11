8d6c6d02e3e1e06423f637f992925a216b5bd923
Thada Wangthammang <thada.wth@gmail.com>
2022-04-11 11:43:05 +0700

    init project

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # actions-yq-secret
 GitHub Action yq wrapper for hiding secrets
+
+yq is a portable command-line YAML, JSON and XML processor
+
+## Usage 
+- Main Repo: https://github.com/mikefarah/yq/
+- yq documentation: https://mikefarah.gitbook.io/yq/
+
+
---
+++ docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+set -e
+CMD=$1
+SENSITIVE=$2
+echo "::debug::\$cmd: $CMD"
+echo "::debug::\$cmd: $SENSITIVE"
+RESULT=$(eval "$CMD")
+RESULT="${RESULT//'%'/'%25'}"
+RESULT="${RESULT//$'\n'/'%0A'}"
+RESULT="${RESULT//$'\r'/'%0D'}"
+if [ "$SENSITIVE" = "true" ]
+then
+    echo "::add-mask::$RESULT"
+else
+    echo "::debug::\$RESULT: $RESULT"
+fi
+echo ::set-output name=result::"$RESULT"
